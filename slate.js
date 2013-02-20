fe61c2d258992aa670ac98fe4afa1589f09908c5
Timo Sand <timo.j.sand@gmail.com>
2013-02-20 13:22:37 +0000

    CHANGED: updated slate config

--- slate.js
+++ slate.js
@@ -5,20 +5,22 @@ var monTbolt = "2560x1440";
 var hyper = "ctrl;shift;alt;cmd"
 
 // Operations
-var lapChat = S.op("corner", {
-  "screen" : monLaptop,
-  "direction" : "top-left",
-  "width" : "screenSizeX/9",
-  "height" : "screenSizeY"
+var lapMain = S.op("corner", {
+    "scren" : monLaptop,
+    "direction" : "top-right",
+    "width" : "screenSizeX",
+    "height" : "screenSizeY"
 });
-var lapMain = lapChat.dup({ "direction" : "top-right", "width" : "8*screenSizeX/9" });
+var lapTop = lapMain.dup({ "direction" : "top-right", "height" : "2*screenSizeY/3" });
+
 var tboltFull = S.op("move", {
-  "screen" : monTbolt,
-  "x" : "screenOriginX",
-  "y" : "screenOriginY",
-  "width" : "screenSizeX",
-  "height" : "screenSizeY"
+    "screen" : monTbolt,
+    "x" : "screenOriginX",
+    "y" : "screenOriginY",
+    "width" : "screenSizeX",
+    "height" : "screenSizeY"
 });
+
 var tboltTop = tboltFull.dup({ "height" : "screenSizeY/2" });
 var tboltTopLeft = tboltTop.dup({ "width" : "screenSizeX/2" });
 var tboltTopRight = tboltTopLeft.dup({ "x" : "screenOriginX+screenSizeX/2" });
@@ -31,48 +33,42 @@ var tboltRight = tboltTopRight.dup({ "height" : "screenSizeY" });
 
 // common layout hashes
 var lapMainHash = {
-  "operations" : [lapMain],
-  "ignore-fail" : true,
-  "repeat" : true
-};
-var tboltTopHash  = {
-  "operations" : [tboltTop],
-  "repeat" : true
-};
-var iTermHash = {
-  "operations" : [tboltBottomLeft, tboltBottomMid, tboltBottomRight, lapMain],
-  "sort-title" : true,
-  "repeat-last" : true
-};
-var genBrowserHash = function(regex) {
-  return {
-    "operations" : [function(windowObject) {
-      var title = windowObject.title();
-      if (title !== undefined && title.match(regex)) {
-        windowObject.doOperation(hpRight);
-      } else {
-        windowObject.doOperation(lapMain);
-      }
-    }],
+    "operations" : [lapMain],
     "ignore-fail" : true,
     "repeat" : true
-  };
-}
+};
+var tboltTopHash  = {
+    "operations" : [tboltTop],
+    "repeat" : true
+};
+var iTermHash = {
+    "operations" : [tboltBottomLeft, tboltBottomMid, tboltBottomRight, lapTop],
+    "sort-title" : true,
+    "repeat-last" : true
+};
+
+var lapiTermHash =  {
+    "operations" : [lapMain],
+    "ignore-fail" : true,
+    "operations" : [lapTop],
+    "repeat" : true
+};
 
 // 2 monitor layout
 var twoMonitorLayout = S.lay("twoMonitor", {
-  "iTerm" : iTermHash,
-  "Google Chrome" : lapMainHash,
-  "SourceTree" : lapMainHash,
-  "Spotify" : lapMainHash
+    "iTerm" : iTermHash,
+    "Google Chrome" : lapMainHash,
+    "SourceTree" : lapMainHash,
+    "Spotify" : lapMainHash
 });
 
 // 1 monitor layout
 var oneMonitorLayout = S.lay("oneMonitor", {
-  "iTerm" : lapMainHash,
-  "Google Chrome" : lapMainHash,
-  "SourceTree" : lapMainHash,
-  "Spotify" : lapMainHash
+    "iTerm" : lapiTermHash,
+    "Sublime Text 2" : lapMainHash,
+    "Google Chrome" : lapMainHash,
+    "SourceTree" : lapMainHash,
+    "Spotify" : lapMainHash
 });
 
 S.def([monTbolt, monLaptop], twoMonitorLayout);
@@ -82,25 +78,25 @@ S.def([monLaptop], oneMonitorLayout);
 var twoMonitor = S.op("layout", { "name" : twoMonitorLayout });
 var oneMonitor = S.op("layout", { "name" : oneMonitorLayout });
 var universalLayout = function() {
-  // Should probably make sure the resolutions match but w/e
-  if (S.screenCount() === 3) {
-    threeMonitor.run();
-  } else if (S.screenCount() === 2) {
-    twoMonitor.run();
-  } else if (S.screenCount() === 1) {
-    oneMonitor.run();
-  }
+    // Should probably make sure the resolutions match but w/e
+    if (S.screenCount() === 3) {
+        threeMonitor.run();
+    } else if (S.screenCount() === 2) {
+        twoMonitor.run();
+    } else if (S.screenCount() === 1) {
+        oneMonitor.run();
+    }
 };
 
 // Batch bind everything. Less typing.
 S.bnda({
 
-  // Layout Bindings
-  "padEnter:ctrl" : universalLayout,
-  "space:ctrl" : universalLayout,
+    // Layout Bindings
+    "padEnter:ctrl" : universalLayout,
+    "space:ctrl" : universalLayout,
 
-  // Relaunch
-  "r:ctrl;shift;alt;cmd" : S.op("relaunch")
+    // Relaunch
+    "r:ctrl;shift;alt;cmd" : S.op("relaunch")
 });
 
 // Log that we're done configuring
