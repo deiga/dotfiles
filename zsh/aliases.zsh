2d1ec38a9cae36b06613e64950e78c953d2919ff
Timo Sand <timo.j.sand@gmail.com>
2013-03-01 14:05:52 +0000

    Added loads more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,12 +6,44 @@ then
   alias git=$hub_path
 fi
 
+# Detect which `ls` flavor is in use
+if ls --color > /dev/null 2>&1; then # GNU `ls`
+  colorflag="--color"
+else # OS X `ls`
+  colorflag="-G"
+fi
+
 # ls aliases
-alias ls='ls $LS_OPTIONS -hF'
-alias la='ls $LS_OPTIONS -lhAF'
-alias ll='ls $LS_OPTIONS -lhF'
+alias ls="ls ${colorflag} -hF"
+alias la="ls ${colorflag} -lhAF"
+alias ll="ls ${colorflag} -lhF"
 
 # added interactive and verbose mode for common file operations
 alias cp="cp -iv"
 alias mv="mv -iv"
 alias rm="rm -vi"
+
+# Enable aliases to be sudo’ed
+alias sudo='sudo '
+
+# Flush Directory Service cache
+alias flush="dscacheutil -flushcache && killall -HUP mDNSResponder"
+
+# Clean up LaunchServices to remove duplicates in the “Open With” menu
+alias lscleanup="/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user && killall Finder"
+
+# Empty the Trash on all mounted volumes and the main HDD
+# Also, clear Apple’s System Logs to improve shell startup speed
+alias emptytrash="sudo rm -rfv /Volumes/*/.Trashes; sudo rm -rfv ~/.Trash; sudo rm -rfv /private/var/log/asl/*.asl"
+
+# One of @janmoesen’s ProTip™s
+for method in GET HEAD POST PUT DELETE TRACE OPTIONS; do
+  alias "$method"="lwp-request -m '$method'"
+done
+
+alias reload="source ~/.zshrc"
+
+# Stuff I never really use but cannot delete either because of http://xkcd.com/530/
+alias stfu="osascript -e 'set volume output muted true'"
+alias pumpitup="osascript -e 'set volume 7'"
+alias hax="growlnotify -a 'Activity Monitor' 'System error' -m 'WTF R U DOIN'"
