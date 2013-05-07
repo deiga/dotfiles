e933c2c2cf5e40862b3c308f491740977c2652b1
Timo Sand <timo.j.sand@gmail.com>
2013-05-07 14:42:18 +0100

    Removed gathering of themes

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -13,9 +13,6 @@ growl() {
 
 }
 
-# Add good theme to list
-good_theme() { echo $@ >> ~/dotfiles/config/zsh_themes }
-
 # all-round man
 man () {
 /usr/bin/man $@ || (help $@ 2> /dev/null && help $@ | less)
