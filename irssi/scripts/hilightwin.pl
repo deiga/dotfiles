e6586ff587aa299a73570fb73ce8c513ba3ef047
Timo Sand <timo.j.sand@gmail.com>
2013-07-10 16:22:01 +0300

    Updated version

--- irssi/scripts/hilightwin.pl
+++ irssi/scripts/hilightwin.pl
@@ -1,36 +1,57 @@
-# Print hilighted messages & private messages to window named "hilight"
-# for irssi 0.7.99 by Timo Sirainen
+#
+# Print hilighted messages & private messages to window named "hilight" for
+# irssi 0.7.99 by Timo Sirainen
+#
+# Modded a tiny bit by znx to stop private messages entering the hilighted
+# window (can be toggled) and to put up a timestamp.
+#
+
 use Irssi;
+use POSIX;
 use vars qw($VERSION %IRSSI); 
-$VERSION = "0.01";
+
+$VERSION = "0.02";
 %IRSSI = (
-    authors	=> "Timo \'cras\' Sirainen",
-    contact	=> "tss\@iki.fi", 
-    name	=> "hilightwin",
-    description	=> "Print hilighted messages & private messages to window named \"hilight\"",
-    license	=> "Public Domain",
-    url		=> "http://irssi.org/",
-    changed	=> "2002-03-04T22:47+0100"
+    authors     => "Timo \'cras\' Sirainen, Mark \'znx\' Sangster",
+    contact     => "tss\@iki.fi, znxster\@gmail.com", 
+    name        => "hilightwin",
+    description => "Print hilighted messages to window named \"hilight\"",
+    license     => "Public Domain",
+    url         => "http://irssi.org/",
+    changed     => "Sun May 25 18:59:57 BST 2008"
 );
 
 sub sig_printtext {
-  my ($dest, $text, $stripped) = @_;
+    my ($dest, $text, $stripped) = @_;
 
-  # change to (MSGLEVEL_HILIGHT|MSGLEVEL_MSGS) to work for
-  # all privmsgs too
-  if (($dest->{level} & (MSGLEVEL_HILIGHT)) &&
-      ($dest->{level} & MSGLEVEL_NOHILIGHT) == 0) {
-    $window = Irssi::window_find_name('hilight');
+    my $opt = MSGLEVEL_HILIGHT;
 
-    if ($dest->{level} & MSGLEVEL_PUBLIC) {
-      $text = $dest->{target}.": ".$text;
+    if(Irssi::settings_get_bool('hilightwin_showprivmsg')) {
+        $opt = MSGLEVEL_HILIGHT|MSGLEVEL_MSGS;
     }
-    $text =~ s/%/%%/g;
-    $window->print($text, MSGLEVEL_CLIENTCRAP) if ($window);
-  }
+    
+    if(
+        ($dest->{level} & ($opt)) &&
+        ($dest->{level} & MSGLEVEL_NOHILIGHT) == 0
+    ) {
+        $window = Irssi::window_find_name('hilight');
+        
+        if ($dest->{level} & MSGLEVEL_PUBLIC) {
+            $text = $dest->{target}.": ".$text;
+        }
+        $text = strftime(
+            Irssi::settings_get_str('timestamp_format')." ",
+            localtime
+        ).$text;
+        $window->print($text, MSGLEVEL_NEVER) if ($window);
+    }
 }
 
 $window = Irssi::window_find_name('hilight');
 Irssi::print("Create a window named 'hilight'") if (!$window);
 
+Irssi::settings_add_bool('hilightwin','hilightwin_showprivmsg',1);
+
 Irssi::signal_add('print text', 'sig_printtext');
+
+# vim:set ts=4 sw=4 et:
