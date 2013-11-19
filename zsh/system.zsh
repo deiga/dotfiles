b3390d072582b289939a3bb4603a9f147e48fd91
Timo Sand <timo.j.sand@gmail.com>
2013-11-19 20:41:06 +0200

    Some fixes

--- zsh/system.zsh
+++ zsh/system.zsh
@@ -6,13 +6,13 @@ case $OSTYPE in
         alias manpdf="man -t $0 | ps2pdf - - | open -f -a Preview"
 
         # Use keychain for HTTPS git
-        git config --global credential.helper osxkeychain
+        git config --global credential.helper osxkeychain &
 
         source ~/bin/osx_functions.sh
 
         ;;
     *)
         # Cache credentials for 60min for HTTPS git
-        git config credential.helper 'cache --timeout=3600'
+        git config credential.helper 'cache --timeout=3600' &
         ;;
 esac
