8b2e31a1c7b40b4a7f9035d23d44e0191891d59b
Timo Sand <timo.j.sand@gmail.com>
2015-12-25 00:44:32 +0200

    ZSH Adds hook to prepend current folders node_modules/.bin to PATH

---
+++ zsh/init/npm_bin_to_path.zsh
@@ -0,0 +1,13 @@
+# Adds node_modules/.bin to the PATH
+npm_chpwd_hook() {
+    if [ -n "${PRENPMPATH+x}" ]; then
+        PATH=$PRENPMPATH
+        unset PRENPMPATH
+    fi
+    if [ -f package.json ]; then
+        PRENPMPATH=$PATH
+        PATH=$(npm bin):$PATH
+    fi
+}
+
+add-zsh-hook preexec npm_chpwd_hook
