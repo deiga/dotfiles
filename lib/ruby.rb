897d4460c377eef4dc7aa48429872ce8f95cd0b0
Timo Sand <timo.sand@iki.fi>
2017-04-04 18:04:11 +0300

    Removes RVM stuff

--- lib/ruby.rb
+++ lib/ruby.rb
@@ -1,23 +1,5 @@
 require_relative 'common'
 
-
-# Method to install RVM (Ruby Version Manager)
-def install_rvm
-    %x{which rvm}
-    unless $?.success?
-        LOGGER.info "\nInstalling RVM".blue
-        autolibs = OSX ? 'homebrew' : 'packages'
-        system %Q{curl -L https://get.rvm.io | bash -s stable --autolibs=#{autolibs} --ruby --with-gems="pry gem-ctags git-up compass gem-browse httparty pry-plus" --without-gems=rubygems-bundler}
-        system 'rvm autolibs homebrew' if OSX
-    end
-end
-
-def uninstall_rvm
-    `/usr/bin/sudo rm -rf $HOME/.rvm $HOME/.rvmrc /etc/rvmrc /etc/profile.d/rvm.sh /usr/local/rvm /usr/local/bin/rvm`
-    `/usr/bin/sudo /usr/sbin/groupdel rvm`
-    LOGGER.info 'RVM is removed. Please check all .bashrc|.bash_profile|.profile|.zshrc for RVM source lines and delete or comment out if this was a Per-User installation.'.blue
-end
-
 def install_rbenv
   if OSX
     system 'brew install rbenv ruby-build rbenv-readline'
