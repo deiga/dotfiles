1160b28a482f89aa773b000eadca159386a21285
Timo Sand <timo.sand@iki.fi>
2017-04-08 15:00:01 +0300

    Fixes errors in bootstrapping script

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -6,22 +6,21 @@ set -u
 
 echo "Running 'xcode-select --install'"
 xcode-select --install || true
-sleep 1
 
 echo "Sleeping for 30sec"
-sleep 1m
+sleep 30s
 
 echo "Cloning into ~/dotfiles"
 git clone https://github.com/deiga/dotfiles.git ~/dotfiles
 
 cd ~/dotfiles
 
-osascript -e 'tell application "System Events" to click (every button whose value of attribute "AXDescription" is "add desktop") of group 1 of process "Dock"' # Ask for assistive access
+echo "Ask for assistive access"
+osascript -e 'tell application "System Events" to click (every button whose value of attribute "AXDescription" is "add desktop") of group 1 of process "Dock"' || true
 osascript -e 'tell app "System Preferences"' -e 'activate' -e 'set current pane to pane "com.apple.preference.security"' -e 'end tell'
-osascript -e 'tell app "Finder" to display dialog "Please accept assistive access for iTerm"'
 
 echo "Sleeping for 30sec"
-sleep 30
+sleep 30s
 
 echo "Running rake install"
 rake install
