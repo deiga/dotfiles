3ea9f22c0a458121ba313eb44ac9076b435dab06
Timo Sand <timo.sand@iki.fi>
2017-06-08 13:11:37 +0300

    Changes to actually update all installed pip packages

--- lib/python.rb
+++ lib/python.rb
@@ -13,6 +13,6 @@ def install_python
 end
 
 def update_python
-    system %(PIP_REQUIRE_VIRTUALENV="" pip install --upgrade pip setuptools wheel virtualenv virtualenvwrapper mercurial psutil==1.2.1 ansible-lint)
+    system %(PIP_REQUIRE_VIRTUALENV="" pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | PIP_REQUIRE_VIRTUALENV="" xargs -n1 pip install -U)
     system %{LIBGIT2="$HOME/local" LDFLAGS="-Wl,-rpath='$LIBGIT2/lib',--enable-new-dtags $LDFLAGS" pip install -U pygit2} unless OSX
 end
