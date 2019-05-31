7fcf5d1926e79cc55b7663e11991ffa9dfe294fa
Timo Sand <timo.sand@iki.fi>
2019-06-01 01:31:01 +0300

    Removes unused code

--- bin/osx_functions.sh
+++ bin/osx_functions.sh
@@ -4,14 +4,6 @@ update_hosts () {
     curl http://someonewhocares.org/hosts/hosts | sudo tee /etc/hosts.whocare > /dev/null
 }
 
-parents_wifi () {
-    /usr/sbin/networksetup -setairportpower Wi-Fi off
-    sleep 1
-    /usr/sbin/networksetup -setairportpower Wi-Fi on
-    sleep 3
-    /usr/sbin/networksetup -setairportnetwork Wi-Fi JASWIFI detlef23
-}
-
 current_wallpaper() {
     defaults read com.apple.desktop Background |grep LastName | awk '{print $3}'| sed -e 's/;//g' | sed -e 's/"//g' | uniq
 }
