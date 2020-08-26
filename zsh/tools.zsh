9d6e2a0848408199fe310ac4a6a307f39b701a1f
Timo Sand <timo.sand@iki.fi>
2020-08-26 22:38:49 +0200

    Update asdf config
    Signed-off-by: Timo Sand <timo.sand@iki.fi>

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -1,11 +1,8 @@
 #!/bin/sh
-# local ASDF_DIR=$(brew --prefix asdf)
-# In order to bypass asdf shims. We *only* add the `ASDF_DIR/bin`
-# directory to PATH, since we still want to use `asdf` but not its shims.
-# [[ $PATH == *"asdf/bin"* ]] || export PATH="$PATH:ASDF_DIR/bin"
-# . $ASDF_DIR/lib/asdf.sh
-. $(brew --prefix asdf)/asdf.sh
-# [[ $PATH == *"direnv"* ]] || local DIRENV_PATH=$(dirname $(asdf which direnv)) && export PATH="$DIRENV_PATH:$PATH"
+
+# Dont source `~/.asdf/asdf.sh`
+PATH="$PATH:$BREW_PREFIX/opt/asdf/bin"
+source "$BREW_PREFIX/opt/asdf/lib/asdf.sh" # just load the asdf wrapper function
 
 # Hook direnv into your shell.
 eval "$(asdf exec direnv hook zsh)"
