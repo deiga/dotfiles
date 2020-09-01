85b69a7784cf3121f6fc8be310f17737027d4c3d
Timo Sand <timo.sand@iki.fi>
2020-09-01 22:40:50 +0200

    Move exports to envrc
    Signed-off-by: Timo Sand <timo.sand@iki.fi>

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,17 +1,1 @@
-export WORKON_HOME=$HOME/Dropbox/.virtualenvs
-export PROJECT_HOME=$HOME/Dropbox/Documents/Projects
-export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
-export PYTHONSTARTUP=$HOME/.pythonrc
-
-export ENHANCD_COMMAND=ecd
-export EDITOR=vim
-export VISUAL=code
-
-export COMPOSE_DOCKER_CLI_BUILD=1
-export DOCKER_BUILDKIT=1
-export CLOUDSDK_PYTHON=python
-
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules}/*" 2> /dev/null'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-
 if [ -f $HOME/Dropbox/Avaimet/shell/secrets.sh ]; then source $HOME/Dropbox/Avaimet/shell/secrets.sh; fi
