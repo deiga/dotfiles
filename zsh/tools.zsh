0a77efc16dac6ece2cccb4683203ade191292a5d
Timo Sand <timo.sand@iki.fi>
2020-11-08 20:51:46 +0100

    Add paths for gcloud sdk
    Signed-off-by: Timo Sand <timo.sand@iki.fi>

--- zsh/tools.zsh
+++ zsh/tools.zsh
@@ -38,3 +38,8 @@ direnv() { asdf exec direnv "$@"; }
 if type fasd >/dev/null; then
   eval "$(fasd --init auto)"
 fi
+
+if [ -d "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/" ]; then
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/path.zsh.inc"
+  source "/usr/local/Caskroom/google-cloud-sdk/latest/google-cloud-sdk/completion.zsh.inc"
+fi
