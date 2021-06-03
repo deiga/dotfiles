ff8b9895ea728560334fa2481f430d9194563480
Timo Sand <timo.sand@iki.fi>
2021-06-03 18:33:36 +0300

    Update to use asdf hooks
    Signed-off-by: Timo Sand <timo.sand@iki.fi>

--- zsh/init/npm_bin_to_path.zsh
+++ zsh/init/npm_bin_to_path.zsh
@@ -1,13 +1,13 @@
 # Adds node_modules/.bin to the PATH
 npm_chpwd_hook() {
-    if [ -n "${PRENPMPATH+x}" ]; then
-        PATH=$PRENPMPATH
-        unset PRENPMPATH
-    fi
-    if [ -f package.json ]; then
-        PRENPMPATH=$PATH
-        PATH=$(npm bin):$PATH
-    fi
+  if [ -n "${PRENPMPATH+x}" ]; then
+    PATH=$PRENPMPATH
+    unset PRENPMPATH
+  fi
+  if [ -f package.json ]; then
+    PRENPMPATH=$PATH
+    PATH=$($(asdf which npm) bin):$PATH
+  fi
 }
 
 add-zsh-hook preexec npm_chpwd_hook
